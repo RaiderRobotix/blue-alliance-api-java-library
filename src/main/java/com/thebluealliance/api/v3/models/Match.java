21c9c9dd6b19ea1c9d224804ecf4f61462ab33ab
Spencer Ng <sng1488@gmail.com>
2018-03-03 11:21:09 -0500

    Reorganize repo

--- src/main/java/com/thebluealliance/api/v3/models/Match.java
+++ src/main/java/com/thebluealliance/api/v3/models/Match.java
@@ -1,31 +1,17 @@
-package com.thebluealliance.api.v3.models;
+package com.thebluealliance.models;
 
-/** Represents a match played during an event
- *
- */
 public class Match extends SimpleMatch {
 
 	private long post_result_time;
-	private Object score_breakdown;
+	private ScoreBreakdown score_breakdown;
 	private Media[] videos;
 	
-	/**
-	 * @return UNIX timestamp (seconds since 1-Jan-1970 00:00:00) when the match result was posted.
-	 */
 	public long getPostResultTime() {
 		return post_result_time;
 	}
-	
-	/**
-	 * @return Score breakdown for auto, teleop, etc. points. Varies from year to year. May be null.
-	 */
-	public Object getScoreBreakdown() {
+	public ScoreBreakdown getScoreBreakdown() {
 		return score_breakdown;
 	}
-	
-	/**
-	 * @return Array of {@link Media} objects associated with this match.
-	 */
 	public Media[] getVideos() {
 		return videos;
 	}
