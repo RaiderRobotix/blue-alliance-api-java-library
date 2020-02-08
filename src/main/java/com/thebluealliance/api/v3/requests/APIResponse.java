ae397aef80c20978011a12383b38f6da8ed82d14
hybras <24651269+hybras@users.noreply.github.com>
2020-02-08 12:50:49 -0500

    Lombok ApiResponse

--- src/main/java/com/thebluealliance/api/v3/requests/APIResponse.java
+++ src/main/java/com/thebluealliance/api/v3/requests/APIResponse.java
@@ -1,51 +1,24 @@
 package com.thebluealliance.api.v3.requests;
 
+import lombok.Value;
+
 /**
- * Holds a response from The Blue Alliance API com.thebluealliance.api.v3
+ * A response from The Blue Alliance API com.thebluealliance.api.v3
  */
-public class APIResponse {
+@Value public class APIResponse {
 
-    private final String jsonContent;
-	private final String lastModified;
-    private final int responseCode;
-
-    /**
-     * Creates an APIResponse object
-     *
-     * @param jsonContent  JSON data returned by the API
-     * @param lastModified The <code>Last-Modified</code> response header
-     * @param responseCode The HTTP response code
-     */
-    public APIResponse(String jsonContent, String lastModified, int responseCode) {
-        this.jsonContent = jsonContent;
-        this.lastModified = lastModified;
-        this.responseCode = responseCode;
-    }
-
-    /**
-     * @return The JSON string returned by the API. May be null if the <code>
-     * responseCode</code> is not 200.
-     */
-    public String getJson() {
-        return jsonContent;
-    }
-
-    /**
-     * @return The date and time the data returned was last updated. Used by clients in the `If-Modified-Since`
-     * request header.
-     */
-    public String getLastModified() {
-        return lastModified;
-    }
-
-    /**
-     * @return The HTTP response code given by the API.
-     * Value is 200 if the response is successful, 304 if data was not modified and the <code>If-Modified-Since</code>
-     * header is used, 401 if no TBA com.thebluealliance.api.v3 API Key was provided, or it is not valid, or 404 if
-     * the URL or parameter(s) provided is invalid
-     */
-    public int getResponseCode() {
-        return responseCode;
-    }
-
+	/**
+	 * @return The JSON string returned by the API. May be null if the <code>
+	 * responseCode</code> is not 200.
+	 */
+	String json;
+	/**
+	 * @return The date and time the data returned was last updated. Used by clients in the `If-Modified-Since` request header.
+	 */
+	String lastModified;
+	/**
+	 * @return The HTTP response code given by the API. Value is 200 if the response is successful, 304 if data was not modified and the <code>If-Modified-Since</code> header is used, 401 if no TBA
+	 *         com.thebluealliance.api.v3 API Key was provided, or it is not valid, or 404 if the URL or parameter(s) provided is invalid
+	 */
+	int responseCode;
 }
